bd799eb1177f7abfea92089e617b46eff332d7c8
lafnggl <114120881+lafnggl@users.noreply.github.com>
2023-01-04 19:38:22 -0500

    final

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # sqlalchemy-challenge
+## Overview of Project
+
+### Part 1: Analyze and Explore the Climate Data
+* In this section, you’ll use Python and SQLAlchemy to do a basic climate analysis and data exploration of your climate database. 
+    * Precipitation Analysis
+    * Station Analysis
+
+### Part 2: Design Your Climate App
+* Now that you’ve completed your initial analysis, you’ll design a Flask API based on the queries that you just developed.
+ 
+### Purpose
+
+I am utilizing SQLAlchemy and Python to analyze data from a database in order to get more detailed information about what the pulled data shows. As well as creating an API for querying. 
+
+## Analysis and Challenges
+
+### Analysis of Outcomes Based on Goals
+
+* During this assignment I was able to complete all the goals and objectives for the assignment. I used what I have learned in the class to trouble shoot the code as I went with Jupyter Notebook and made sure I was fully understanding what each part of my code did through labeling, so when an error popped up I knew how to approach the problem.
+
+### Challenges and Difficulties Encountered
+
+* I think the biggest challenge for me was figured out how to do the API creation using FLASK. I hit a lot of errors and road blocks, but breaks and repetition won over and eventually I figured out what I needed to do.
---
+++ SurfsUp/app.py
@@ -0,0 +1,152 @@
+import numpy as np
+
+import sqlalchemy
+import datetime as dt
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify 
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end<br/>"
+        f"Note: to access values between a start and end date enter both dates using format: YYYY-mm-dd/YYYY-mm-dd"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+
+    precipitation_query_results = session.query(Measurement.prcp, Measurement.date).all()
+
+    session.close()
+
+    precipitaton_query_values = []
+    for prcp, date in precipitation_query_results:
+        precipitation_dict = {}
+        precipitation_dict["precipitation"] = prcp
+        precipitation_dict["date"] = date
+        precipitaton_query_values.append(precipitation_dict)
+
+    return jsonify(precipitaton_query_values) 
+
+@app.route("/api/v1.0/stations")
+def station(): 
+
+    session = Session(engine)
+
+    station_query_results = session.query(Station.station,Station.id).all()
+
+    session.close()  
+    
+    stations_values = []
+    for station, id in station_query_results:
+        stations_values_dict = {}
+        stations_values_dict['station'] = station
+        stations_values_dict['id'] = id
+        stations_values.append(stations_values_dict)
+    return jsonify (stations_values) 
+
+@app.route("/api/v1.0/tobs") 
+def tobs():
+    session = Session(engine)
+    
+    last_year_query_results = session.query(Measurement.date).\
+        order_by(Measurement.date.desc()).first() 
+
+    print(last_year_query_results)
+    last_year_query_values = []
+    for date in last_year_query_results:
+        last_year_dict = {}
+        last_year_dict["date"] = date
+        last_year_query_values.append(last_year_dict) 
+    print(last_year_query_values)
+    query_start_date = dt.date(2017, 8, 23)-dt.timedelta(days =365) 
+    print(query_start_date) 
+
+    active_station= session.query(Measurement.station, func.count(Measurement.station)).\
+        order_by(func.count(Measurement.station).desc()).\
+        group_by(Measurement.station).first()
+    most_active_station = active_station[0] 
+
+    session.close() 
+    print(most_active_station)
+
+    dates_tobs_last_year_query_results = session.query(Measurement.date, Measurement.tobs, Measurement.station).\
+        filter(Measurement.date > query_start_date).\
+        filter(Measurement.station == most_active_station) 
+    
+    dates_tobs_last_year_query_values = []
+    for date, tobs, station in dates_tobs_last_year_query_results:
+        dates_tobs_dict = {}
+        dates_tobs_dict["date"] = date
+        dates_tobs_dict["tobs"] = tobs
+        dates_tobs_dict["station"] = station
+        dates_tobs_last_year_query_values.append(dates_tobs_dict)
+        
+    return jsonify(dates_tobs_last_year_query_values) 
+
+
+@app.route("/api/v1.0/<start>")
+def start_date(start):
+    session = Session(engine) 
+
+    start_date_tobs_results = session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()
+    
+    session.close() 
+
+    start_date_tobs_values =[]
+    for min, avg, max in start_date_tobs_results:
+        start_date_tobs_dict = {}
+        start_date_tobs_dict["min"] = min
+        start_date_tobs_dict["average"] = avg
+        start_date_tobs_dict["max"] = max
+        start_date_tobs_values.append(start_date_tobs_dict)
+    
+    return jsonify(start_date_tobs_values)
+
+
+@app.route("/api/v1.0/<start>/<end>")
+
+def Start_end_date(start, end):
+    session = Session(engine)
+
+
+    start_end_date_tobs_results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+
+    session.close()
+  
+    start_end_tobs_date_values = []
+    for min, avg, max in start_end_date_tobs_results:
+        start_end_tobs_date_dict = {}
+        start_end_tobs_date_dict["min_temp"] = min
+        start_end_tobs_date_dict["avg_temp"] = avg
+        start_end_tobs_date_dict["max_temp"] = max
+        start_end_tobs_date_values.append(start_end_tobs_date_dict) 
+    
+
+    return jsonify(start_end_tobs_date_values)
+   
+if __name__ == '__main__':
+    app.run(debug=True)
